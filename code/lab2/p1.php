e19e1135e790a86250e1fbe14abd69ec36c86cf0
DarksideCode <darksidecode@ya.ru>
2022-02-05 20:25:19 +0200

    Solve p1

---
+++ code/lab2/p1.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name="15 chicken wings";
+
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= " and a large soda";
+
+// Don't change the line below
+echo"\nYour order is:$very_bad_unclear_name.";
