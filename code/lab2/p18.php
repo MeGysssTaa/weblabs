880e7cc5b20c947866d1086ecf37666b6dc36ce9
DarksideCode <darksidecode@ya.ru>
2022-02-06 13:06:02 +0200

    Solve p18

---
+++ code/lab2/p18.php
@@ -0,0 +1,44 @@
+<?php /** @noinspection PhpConditionAlreadyCheckedInspection */
+
+
+$test = 0;
+
+// Daniel
+if ($test == 0) {
+    echo nl2br("correct\n");;
+}
+
+// The cooler Daniel
+echo $test == 0 ? nl2br("correct\n") : "";
+
+$age = 19;
+
+if ($age < 10)
+    echo nl2br("Too young!\n");
+else if ($age > 99)
+    echo nl2br("Too old!\n");
+else {
+    $digitsSum = 0;
+
+    while ($age > 0) {
+        $digitsSum += $age % 10;
+        $age /= 10;
+    }
+
+    echo nl2br("Sum of digits in your age consists of "
+        . ($age <= 9 ? "one digit" : "two digits")
+        . "\n");
+}
+
+$arr = [1, 2, 3];
+
+if (count($arr) === 3)
+    echo array_sum($arr);
+
+function isSumGt10(int $a, int $b): bool {
+    return $a + $b > 10;
+}
+
+function equals(int $a, int $b): bool {
+    return $a === $b;
+}
