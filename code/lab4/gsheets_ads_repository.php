76996128f603663a606a0362a499a742f0779cee
DarksideCode <darksidecode@ya.ru>
2022-03-07 13:06:07 +0200

    Abstract away Ovito view

---
+++ code/lab4/gsheets_ads_repository.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "../lab3/task3/ads.php";
+
+class GoogleSheetsAdsRepository implements AdsRepository {
+    public function listCategories(): array {
+
+    }
+
+    public function listAds(string $category): array {
+
+    }
+
+    public function saveAd(Ad $ad): bool {
+
+    }
+}
